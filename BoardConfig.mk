98f0da5cfa4520e5e860eb0f5a1a8e9f932ecdd6
kwiksilver <kwiksilver@gmail.com>
2020-03-03 19:47:32 -0500

    updated boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,49 +1,12 @@
-#
-# Copyright (C) 2018 The TwrpBuilder Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#Config
 ALLOW_MISSING_DEPENDENCIES := true
 LOCAL_PATH := device/motorola/surfna
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/twrp.fstab
 DEVICE_PATH := device/motorola/surfna
+DEFCONFIG_PATH := kernel/motorola/surfna/arch/arm64/configs/ext_config
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOOTLOADER_BOARD_NAME := surfna
-
-# Recovery
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 50331648
-BOARD_FLASH_BLOCK_SIZE := 0
-BOARD_HAS_NO_REAL_SDCARD := false
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_USES_MMCUTILS := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-DEVICE_RESOLUTION := 720x1440
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1440
-TW_USE_TOOLBOX := true
-TW_USE_NEW_MINADBD := true
-# TWRP
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-ifeq ($(TW_THEME),)
-TW_THEME := portrait_hdpi
-endif
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
 
 
 # Architecture
@@ -61,14 +24,39 @@ endif
 # Kernel
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
-#TARGET_KERNEL_CONFIG := new-moto-msm8937-surfna_defconfig
+#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
+TARGET_KERNEL_CONFIG := new-moto-msm8937-surfna_defconfig
 #BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_serial_dm,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 printk.devkmsg=on buildvariant=user veritykeyid=id:7e4333f9bba00adfe0ede979e28ed1920492b40f androidboot.selinux=permissive
-#BOARD_KERNEL_BASE := 0x80000000
-#BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_serial_dm,0x78B0000 vmalloc=300M firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 printk.devkmsg=on buildvariant=user androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
 #BOARD_KERNEL_IMAGE_NAME := zImage
-#BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 #BOARD_KERNEL_SEPARATED_DT := true
 #BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt dt.img
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 50331648
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := false
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+DEVICE_RESOLUTION := 720x1440
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1440
+TW_USE_TOOLBOX := true
+TW_USE_NEW_MINADBD := true
+
+# TWRP
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+ifeq ($(TW_THEME),)
+TW_THEME := portrait_hdpi
+endif
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
